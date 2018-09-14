06be47bedd15b355f0035a328495c1c48773b39e
davidlo <davidlobandai>
2018-09-14 11:49:34 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }, {
+        test: /\.html$/,
+        use: ["html-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  plugins: [new HtmlWebPackPlugin({title: 'The Minimal React Webpack Babel Setup', template: "./dist/index.html", filename: "./index.html"})]
+};
