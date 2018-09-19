d93604c2d3269ae97f4eb4afa8983b7d2761c74e
davidlo <davidlobandai>
2018-09-19 15:08:36 +0800

    add example

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import SimpleLogin from './example/SimpleLogin'
+import TwoWayBinding from './example/TwoWayBinding'
+class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (<div></div>)
+  }
+}
+export default App
