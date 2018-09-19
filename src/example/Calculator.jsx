7dba9f39b7c46ba596ba8d3cf441f612cda856f6
UMIQLO <umiqlo.moe@gmail.com>
2018-09-19 22:13:39 +0800

    update calculator

--- src/example/Calculator.jsx
+++ src/example/Calculator.jsx
@@ -3,14 +3,84 @@ import ReactDOM from 'react-dom'
 import LengthInput from './LengthInput'
 
 class Calculator extends React.Component {
-  constructor() {
-    super()
-  }
-  render() {
-    return (<div>
-      <LengthInput scale="CM"></LengthInput>
-      <LengthInput scale="M"></LengthInput>
-    </div>)
-  }
+    constructor() {
+        super()
+        this.state = {
+            length: "",
+            scale: "",
+        }
+        this.handleCenterMeterChange = this.handleCenterMeterChange.bind(this)
+        this.handleMeterChange = this.handleMeterChange.bind(this)
+        this.handleMillimeterChange = this.handleMillimeterChange.bind(this)
+        this.handleKiloMeterChange = this.handleKiloMeterChange.bind(this)
+        this.convert = this.convert.bind(this)
+    }
+
+    handleMillimeterChange(length) {
+        this.setState({
+            scale: "mm",
+            length: length
+        })
+    }
+
+    handleCenterMeterChange(length) {
+        this.setState({
+            scale: "cm",
+            length: length
+        })
+    }
+
+    handleMeterChange(length) {
+        this.setState({
+            scale: "m",
+            length: length
+        })
+    }
+
+    handleKiloMeterChange(length) {
+        this.setState({
+            scale: "km",
+            length: length
+        })
+    }
+
+    convert() {
+        const length = this.state.length
+        const scale = this.state.scale
+        let mm = 0, cm = 0, m = 0, km = 0
+        if (scale === 'mm') {
+            mm = length
+            cm = mm / 10
+            m = cm / 100
+            km = m / 1000
+        } else if (scale === 'cm') {
+            cm = length
+            mm = cm * 10
+            m = cm / 100
+            km = m / 1000
+        } else if (scale === 'm') {
+            m = length
+            cm = m * 100
+            mm = cm * 10
+            km = m / 1000
+        } else if (scale === 'km') {
+            km = length
+            m = km * 1000
+            cm = m * 100
+            mm = cm * 10
+        }
+        return {mm: mm, cm: cm, m: m, km: km}
+    }
+
+    render() {
+        const value = this.convert()
+        return (<div>
+            <LengthInput scale="MM" length={value.mm} onLengthChange={this.handleMillimeterChange}/>
+            <LengthInput scale="CM" length={value.cm} onLengthChange={this.handleCenterMeterChange}/>
+            <LengthInput scale="M" length={value.m} onLengthChange={this.handleMeterChange}/>
+            <LengthInput scale="KM" length={value.km} onLengthChange={this.handleKiloMeterChange}/>
+        </div>)
+    }
 }
+
 export default Calculator
--- src/example/LengthInput.jsx
+++ src/example/LengthInput.jsx
@@ -2,28 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class LengthInput extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      length: ""
+    constructor() {
+        super()
+        this.handleChange = this.handleChange.bind(this)
     }
-    this.handleChange = this.handleChange.bind(this)
-  }
 
-  handleChange(e) {
-    this.setState({length: e.target.value})
-  }
+    handleChange(e) {
+        this.props.onLengthChange(e.target.value)
+    }
 
-  render() {
-    const length = this.state.length
-    const scale = this.props.scale
-    return (<div>
-      <fieldset>
-        <legend>{scale}</legend>
-        <input type="text" name="length" value={length} onChange={this.handleChange}/>
-      </fieldset>
-    </div>)
-  }
+    render() {
+        const length = this.props.length
+        const scale = this.props.scale
+        return (<div>
+            <fieldset>
+                <legend>{scale}</legend>
+                <input type="text" name="length" value={length} onChange={this.handleChange}/>
+            </fieldset>
+        </div>)
+    }
 }
 
 export default LengthInput
