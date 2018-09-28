77919ddcdb613e3f978d5bbcaf8220632f4a40a8
davidlo <davidlobandai>
2018-09-28 17:28:30 +0800

    add login form

--- src/MainApp/components/MainApp.jsx
+++ src/MainApp/components/MainApp.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Navbar'
 import CounterContainer from '../../Counter/containers/CounterContainer'
 import TodoApp from '../../TodoApp/components/TodoApp'
+import LoginForm from '../components/LoginForm'
 import {HashRouter as Router, Switch, Route, NavLink} from 'react-router-dom'
 import {Container, Header} from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css';
@@ -22,7 +23,7 @@ class MainApp extends React.Component {
                         <Route exact={true} path='/' component={home}/>
                         <Route path='/todo' component={TodoApp}/>
                         <Route path='/counter' component={CounterContainer}/>
-                        <Route path='/login' component={login}/>
+                        <Route path='/login' component={LoginForm}/>
                     </Switch>
                 </Container>
             </div>
@@ -33,9 +34,4 @@ class MainApp extends React.Component {
 const home = () => (<Header size='medium'>
     Semantic UI React App
 </Header>)
-
-const login = () => (<Header size='medium'>
-    Login Page
-</Header>)
-
 export default MainApp
