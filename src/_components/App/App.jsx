037dcad3e0b2035b4a6581cfd44b682baa815541
davidlo <davidlobandai>
2018-10-02 15:42:13 +0800

    change file structure

--- src/_components/App/App.jsx
+++ src/_components/App/App.jsx
@@ -1,41 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {connect} from 'react-redux'
-import {clickCount} from '../../_actions'
+import React, {Component} from 'react'
+import {Demo} from '../../_containers/DemoData'
 
-class App extends React.Component {
-    constructor(props) {
-        super(props)
-        this.onAddClickCount = this.onAddClickCount.bind(this)
-    }
-
-    onAddClickCount() {
-        this.props.addClickCount()
-    }
-
+export default class App extends Component {
     render() {
-        return (<div>
-            <p>
-                Hello World (Click Count: {this.props.clickCount})
-            </p>
-            <p><input type="submit" value="Click Me" onClick={this.onAddClickCount}/></p>
-        </div>)
+        return (<Demo/>)
     }
-}
-
-const mapStateToProps = state => {
-    return {clickCount: state.demo.clickCount}
-}
-
-const mapDispatchToProps = dispatch => {
-    return {
-        addClickCount: () => {
-            dispatch(clickCount())
-        }
-    }
-}
-
-const connected = connect(mapStateToProps, mapDispatchToProps)(App)
-export {
-    connected as App
 }
