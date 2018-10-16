b66cd086465ee19d5d0e46a3e3b8b6be3b345e60
davidlo <davidlobandai>
2018-10-16 15:47:13 +0800

    add basic drag & drop

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React, {Component} from 'react'
 import {Demo} from './_components/Demo'
+import {DrapNDrop} from './_components/DrapNDrop'
 
 export default class App extends Component {
     render() {
-        return (<Demo/>)
+        return (<DrapNDrop/>)
     }
 }
